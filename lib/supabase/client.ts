f3dff62424d32ec59f731f7c79d5b2414e02dbce
lauurnce <paneslawrence8@gmail.com>
2026-08-23 19:06:41 +0800

    refactor(supabase): remove unused browser client

--- lib/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { requireEnv } from "./env";
-
-export function createBrowserClient() {
-  return createClient(
-    requireEnv("NEXT_PUBLIC_SUPABASE_URL"),
-    requireEnv("NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY")
-  );
-}
